dee75993157e4b4ee8dec45179cf55527010f12d
A-Wajid <wajid.alphasquad@gmail.com>
2022-04-13 15:12:02 +0500

    fixes'

--- pages/videos/[slug].js
+++ pages/videos/[slug].js
@@ -4,7 +4,7 @@ const Videos =()=>{
         console.log(router.query.slug,'router.query')
     return(
         <div className="flex justify-center items-center min-h-screen bg-slate-400 ">
-        <iframe width="800" height="500" src={`https://www.youtube.com/embed/${router.query.slug}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="800" height="500" src={`https://www.youtube.com/embed/${router.query.slug}`} title="YouTube video player" ></iframe>
         </div>
     )
 }
